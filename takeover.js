1a6d22ceaddc13b8a7195aa91e53bf017eb6b8e2
Samuel Kuld <61301218+SamuelKuld@users.noreply.github.com>
2024-04-07 17:02:11 -0700

    Update takeover.js

--- takeover.js
+++ takeover.js
@@ -1,10 +1,12 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	var init_scan = ns.scan();
-	for (var i of init_scan){
-		try{
-		await ns.wget("https://github.com/SamuelKuld/bihhj/raw/main/takeover.js", "junk.js", i);
-		ns.exec("junk.js", i);}
-		finally{}
+	for (var i of init_scan) {
+		try {
+			ns.nuke(i);
+			await ns.wget("https://github.com/SamuelKuld/bihhj/raw/main/takeover.js", "junk.js", i);
+			ns.exec("junk.js", i);
+		}
+		finally { }
 	}
 }
